ed61a2a7f17b3aca36ef8715d05e2248dfffd722
Hamza Iftikhar <21-CS-45@students.uettaxila.edu.pk>
2023-08-11 18:31:10 +0500

    Assignmnet Day-1 Task-02

---
+++ Day-01/Assignment_Task-02.py
@@ -0,0 +1,12 @@
+# python program to check weather given year is leap year or not 
+
+year = int(input("Enter the year: "))
+
+if year % 4 == 0 and year % 100 != 0:
+    print("It is a leap year")
+elif year % 400 == 0 and year % 100 == 0:
+    print("It is a leap year")  
+else:
+    print("It is not a leap year")
+    
+        
